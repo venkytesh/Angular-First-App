fbc3f8a0aa11443ada9ab63c011f1c1c1dc72952
venkytesh <venkyteshjagdale@gmail.com>
2020-09-14 16:22:44 +0530

    vk-1122 this is the info related to  git

---
+++ welcome.js
@@ -0,0 +1,6 @@
+The course will explain how to set up Git configurations on your local machine. You will also learn about GitHub. In addition to providing familiarity with GitHub, this course will also teach you how to create remote repositories (more on this later) and local repositories. By the end of this course, you should have a solid grasp on several important Git concepts such as:
+
+Commits
+Branches
+Merging
+Rebasing
