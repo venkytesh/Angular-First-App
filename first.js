8f4fa6efc602a6d9db4fa77afcf23c6f993aa2ad
venkytesh <venkyteshjagdale@gmail.com>
2020-09-14 17:32:17 +0530

    remov extra information

--- first.js
+++ first.js
@@ -1,4 +1,2 @@
 What is Git? #
-Git is a distributed version control software. As a result, a complete copy of the entire codebase will be available on every contributor’s computer; we can also call this codebase a local repository.
 
-Git tracks the local repository, maintains a record of all the changes that occur within it. It removes the hassle of keeping multiple versions of the project in separate directories and also makes sharing changes to the source code between collaborators seamless and quick.
