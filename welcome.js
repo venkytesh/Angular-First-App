26de9903f745b49af3e87dd5b7941b09fe068d41
venkytesh <venkyteshjagdale@gmail.com>
2020-09-14 16:34:28 +0530

    add what is version controll

--- welcome.js
+++ welcome.js
@@ -1,6 +1,7 @@
 The course will explain how to set up Git configurations on your local machine. You will also learn about GitHub. In addition to providing familiarity with GitHub, this course will also teach you how to create remote repositories (more on this later) and local repositories. By the end of this course, you should have a solid grasp on several important Git concepts such as:
 
-Commits
-Branches
-Merging
-Rebasing
+Why do we need version control?
+Keep track of changes to the codebase
+Ease in collaborative work
+Version control tools
+
