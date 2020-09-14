03c21ceff8c623a7ef92658e03a291da98f5fb73
venkytesh <venkyteshjagdale@gmail.com>
2020-09-14 17:27:25 +0530

    vj-1998 remove wiki info

--- first.js
+++ first.js
@@ -5,6 +5,4 @@ Git tracks the local repository, maintains a record of all the changes that occu
 
 
 
-India (Hindi: Bhārat), officially the Republic of India (Hindi: Bhārat Gaṇarājya),[23] is a country in South Asia. It is the second-most populous country, the seventh-largest country by land area, and the most populous democracy in the world. Bounded by the Indian Ocean on the south, the Arabian Sea on the southwest, and the Bay of Bengal on the southeast, it shares land borders with Pakistan to the west;[f] China, Nepal, and Bhutan to the north; and Bangladesh and Myanmar to the east. In the Indian Ocean, India is in the vicinity of Sri Lanka and the Maldives; its Andaman and Nicobar Islands share a maritime border with Thailand and Indonesia.
 
-Modern humans arrived on the Indian subcontinent from Africa no later than 55,000 years ago.[24] Their long occupation, initially in varying forms of isolation as hunter-gatherers, has made the re
